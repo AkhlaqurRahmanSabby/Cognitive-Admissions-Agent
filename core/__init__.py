8ab1a0c4bf2901886b75a68f210016c171838528
AkhlaqurRahmanSabby <sabby.rahman@gmail.com>
2026-02-28 19:41:48 -0700

    Initialize core package and expose engine classes

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+from .ai_client import client
+from .interview_engine import InterviewEngine
+from .evaluation_engine import EvaluationEngine
+
+__all__ = [
+    "client",
+    "InterviewEngine",
+    "EvaluationEngine"
+]
